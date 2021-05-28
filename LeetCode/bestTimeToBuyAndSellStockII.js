3893996f7997438c3acfb3a6467033bf9078077e
bchin224 <bchin224@gmail.com>
2021-05-27 21:48:28 -0400

    best time to buy and sell stock

---
+++ LeetCode/bestTimeToBuyAndSellStockII.js
@@ -0,0 +1,15 @@
+Input: prices = [1, 7, 2, 3, 6, 7, 6, 7]
+
+// the profit is the sum of all a peak is found
+// a peak is anytime a number is followed by a smaller number
+var maxProfit = function(prices) {
+    let profit = 0
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1]
+        }
+    }
+    return profit
+};
+
+maxProfit(prices)
