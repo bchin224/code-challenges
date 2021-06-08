30082cbe851e483b7810ae27de429658f2c7c377
bchin224 <bchin224@gmail.com>
2021-06-08 17:12:41 -0400

    design linked list get function

--- LeetCode/LinkedList/designLinkedList.js
+++ LeetCode/LinkedList/designLinkedList.js
@@ -24,14 +24,15 @@ MyLinkedList.prototype.get = function(index) {
     }
     // this.head should always point to a node, not to a specific value
     let currentNode = this.head
+    // console.log('This is currentNode.next.val: ', currentNode.next.val) 
     // console.log('This is currentNode, ', currentNode.val)
     for (let i = 0; i < index; i++) {
         // currentNode.next is just a pointer to the next node
         // still need to use .next.val to target the next value
-        currentNode.val = currentNode.next.val
-        console.log(`At ${i} the currentNode.val is now ${currentNode.val}`)
+        currentNode = currentNode.next
+        // console.log(`At ${i} the currentNode.val is now ${currentNode.val}`)
     }
-    // console.log('At the given index, the node was: ', currentNode.val)
+    console.log(`At ${index}, the node was: ${currentNode.val}`)
     return currentNode.val
 };
 
@@ -74,7 +75,7 @@ MyLinkedList.prototype.addAtTail = function(val) {
         // console.log('This is this.tail after, ', this.tail)
     }
     this.size++
-    console.log(myList)
+    // console.log(myList)
     return this
 };
 
@@ -139,12 +140,11 @@ myList.addAtHead(8)
 // assert(myList.get(1) === 5)
 // assert(myList.get(-1) === -1)
 myList.addAtHead(14)
-assert(myList.get(0) === 14)
-assert(myList.get(1) === 8)
+// assert(myList.get(0) === 14)
+// assert(myList.get(1) === 8)
 // assert(myList.get(2) === 5)
-// myList.addAtTail(80)
-// assert(myList.get(2) === 80)
-// assert(myList.get(3) === 80)
+myList.addAtTail(80)
+assert(myList.get(3) === 80)
 
 // // case 2
 // myList = new MyLinkedList()
