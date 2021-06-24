cf758ec40b8433748bef0214ffdd984cebe4922c
bchin224 <bchin224@gmail.com>
2021-06-23 21:11:58 -0400

    longest common prefix

---
+++ LeetCode/longestCommonPrefix.js
@@ -0,0 +1,20 @@
+Input: strs = ["flower","flow","flight"]
+
+// O(n^2) TC O(1) SC
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return ''
+    
+    const first = strs[0]
+    
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            const other = strs[j]
+            if (other[i] !== first[i]) {
+                return other.slice(0,i)
+            }
+        }
+    }
+    return first
+};
+
+longestCommonPrefix(strs)
